350c51313aca7cd521c2358edbbb2bc1439f53c7
sangeeta27paul <120769381+sangeeta27paul@users.noreply.github.com>
2023-04-04 22:41:31 +0530

    create order and fulfilled endpoints

--- server.js
+++ server.js
@@ -1,11 +1,90 @@
-const express = require("express");
-const app = express();
+
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+// Endpoint to create a new order for a customer
+app.post('/orders/:storeHash', (req, res) => {
+  const storeHash = req.params.storeHash;
+  const accessToken = 'YOUR_ACCESS_TOKEN'; // Replace with your own access token
+
+  // Find the customer with the provided phone number
+  const customerPhoneNumber = req.body.phone;
+  axios.get(`https://api.bigcommerce.com/stores/${storeHash}/v3/customers?query=${customerPhoneNumber}`, {
+    headers: {
+      'X-Auth-Token': accessToken,
+      'Accept': 'application/json'
+    }
+  })
+  .then(customerResponse => {
+    const customer = customerResponse.data.data[0]; // Assume the first result is the correct customer
+    const customerId = customer.id;
+
+    // Create a new order for the customer with the provided products
+    const orderData = {
+      customer_id: customerId,
+      products: req.body.products
+    };
+
+    axios.post(`https://api.bigcommerce.com/stores/${storeHash}/v3/orders`, orderData, {
+      headers: {
+        'X-Auth-Token': accessToken,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(orderResponse => {
+      const orderId = orderResponse.data.data.id;
+      console.log(`Order ${orderId} has been created`);
+      res.status(201).json({ orderId: orderId });
+    })
+    .catch(error => {
+      console.error(`Unable to create order: ${error}`);
+      res.sendStatus(500);
+    });
+  })
+  .catch(error => {
+    console.error(`Unable to find customer with phone number ${customerPhoneNumber}: ${error}`);
+    res.sendStatus(500);
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+
+// Endpoint to update order status when order is fulfilled
+app.put('/orders/:storeHash/:id/fulfill', (req, res) => {
+  const storeHash = req.params.storeHash;
+  const orderId = req.params.id;
+  const accessToken = 'YOUR_ACCESS_TOKEN'; // Replace with your own access token
+
+  // Update the order with the provided ID and set its status to "Shipped"
+  const orderData = {
+    status_id: 4 // "fullfilled" status
+  };
+
+  axios.put(`https://api.bigcommerce.com/stores/${storeHash}/v2/orders/${orderId}`, orderData, {
+    headers: {
+      'X-Auth-Token': accessToken,
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => {
+    console.log(`Order ${orderId} has been updated to order fulfilled status`);
+    res.sendStatus(200);
+  })
+  .catch(error => {
+    console.error(`Unable to update order ${orderId} status: ${error}`);
+    res.sendStatus(500);
+  });
+});
+
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
 });
